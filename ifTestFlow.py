33d20f35190a35f190166c3ba7b9a86815cbe299
Martin <bekzodsanakulov89@gmail.com>
2021-06-25 22:49:10 +0900

    feat: if cases and or

--- ifTestFlow.py
+++ ifTestFlow.py
@@ -1,3 +1,12 @@
 name = input('Type here your name: ')
 age = int(input('When did you born Dear {}? '.format(name)))
 print('Your name is {} and you are {} years old'.format(name, 2021-age))
+
+if 20 <= age < 50:
+    print('have a good working day')
+
+# if 20 <= age & age < 50:
+#     print('have a good working day')
+#
+# if 20 <= age and age < 50:
+#     print('have a good working day')
