e72dda18a8a6bc55e8eb42c92180716a67ea2b3e
Martin <bekzodsanakulov89@gmail.com>
2021-06-25 23:09:30 +0900

    feat: True, False and bool() values

--- ifTestFlow.py
+++ ifTestFlow.py
@@ -1,12 +1,14 @@
-name = input('Type here your name: ')
-age = int(input('When did you born Dear {}? '.format(name)))
-print('Your name is {} and you are {} years old'.format(name, 2021-age))
+if not True:
+    print('Passed here as not true')
 
-if 20 <= age < 50:
-    print('have a good working day')
+if not False:
+    print('Passed here as not False')
 
-# if 20 <= age & age < 50:
-#     print('have a good working day')
-#
-# if 20 <= age and age < 50:
-#     print('have a good working day')
+if bool(''):
+    print("Passed here as '' ")
+
+if bool(5):
+    print("Passed here as 5 ")
+
+if bool(0):
+    print('Passed here as 0 ')
