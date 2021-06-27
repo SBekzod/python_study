2624b0d76e0045817bafa4c1728a9a3cc2024cde
Martin <martin@comets.kr>
2021-06-28 00:53:35 +0900

    feat: all operators

---
+++ operators.py
@@ -0,0 +1,14 @@
+x = 25
+print('this is the x value {0}'.format(x))
+
+x += 5
+print('this is the x value {0}'.format(x))
+
+x -= 25
+print('this is the x value {0}'.format(x))
+
+x **= 4
+print('this is the x value {0}'.format(x))
+
+x %= 60
+print('this is the x value {0}'.format(x))
