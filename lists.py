2fbd7f6044cd5de5041100a52db89563060cbbd4
Martin <martin@comets.kr>
2021-07-05 01:02:21 +0900

    feat: str.split() method and list(str) constructor

--- lists.py
+++ lists.py
@@ -1,17 +1,20 @@
-spe_list = [1, 3, 4, 5, 9, 3, 8]
-ele_max = max(spe_list)
-ele_min = min(spe_list)
+list_1 = []
+list_2 = list()
 
-ele_count = spe_list.count(3)
+print('List one equals {0}'.format(list_1))
+print('List two equals {0}'.format(list_2))
+if list_1 == list_2:
+    print('The lists are equal \n')
+else:
+    print('Not equal \n')
 
-print('In the list, the maximum value is {0} and minimum value is {1}'.format(ele_max, ele_min))
-print('counts of 3 in list equals {0}'.format(ele_count))
 
-even = [2, 4, 8, 12, 20]
-odd = [1, 3, 7, 11, 25]
-numbers = even + odd
-# numbers.sort()
-numbers = sorted(numbers)
-print('The numbers equal to the list of {0}'.format(numbers))
+text = 'you are awesome'
+list_3 = text.split()
+print('THE RESULT ONE: {0}'.format(list_3))
+list_4 = list(text)
+print('THE RESULT TWO: {0}'.format(list_4))
+
+
 
 
