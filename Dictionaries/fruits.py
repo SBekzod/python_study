65465bfb76a9186fc2f3fd369e6330768c562ddf
martin <martin@comets.kr>
2021-10-18 03:40:53 +0900

    fix: go on with fruit dictionary

--- Dictionaries/fruits.py
+++ Dictionaries/fruits.py
@@ -2,16 +2,31 @@ fruits = {'apple': 'red', 'lemon': 'yellow', 'grapes': 'green'}
 print('THE FRUITS ONE:')
 print(fruits)
 
-fruits['orange'] = 'orange'
-print('THE FRUITS TWO:')
-print(fruits)
+# fruits['orange'] = 'orange'
+# print('THE FRUITS TWO:')
+# print(fruits)
+#
+# del fruits['apple']
+# print('THE FRUITS THREE:')
+# print(fruits)
+#
+# fruits.clear()
+# print('THE FRUITS FOUR:')
+# print(fruits)
 
-del fruits['apple']
-print('THE FRUITS THREE:')
-print(fruits)
+while False:
+    search = input('Type the fruit name here: ')
+    if search == 'quit':
+        break
+    if search in fruits:
+        result = fruits.get(search)
+        print(result)
+    else:
+        print('There is no fruit like than on fruits dictionary')
 
-fruits.clear()
-print('THE FRUITS FOUR:')
-print(fruits)
+
+
+
+
 
 
