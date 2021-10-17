5344ac31edee0fc7a909262cb879912c2a94ccd1
martin <martin@comets.kr>
2021-10-18 03:09:56 +0900

    feat: dictionaries in python

---
+++ dictionary_study.py
@@ -0,0 +1,12 @@
+# Dictionaries in python are a map in other languages
+
+dictionary = {"name": "Martin", "age": 33, "nationality": "uzbek", "married": True}
+print('NAME: ' + dictionary['name'])
+
+
+
+
+
+
+
+
