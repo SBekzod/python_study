bc163f97c27a0b97e34927c27272e2ba3e40626d
martin <martin@comets.kr>
2021-11-01 03:04:53 +0900

    feat: working with time core library and time formats

---
+++ FunMod/time_me.py
@@ -0,0 +1,19 @@
+import time
+from time import time as now
+
+print(time.gmtime())
+print('='*40)
+print(time.localtime())
+print('='*40)
+print(time.time())
+print(time.strftime('%X', time.localtime(now())))
+print()
+
+
+print('*'*80)
+curr_time = time.localtime()
+print(curr_time)
+print('YEAR: ', curr_time[0])
+print('MONTH: ', curr_time[1])
+print('DAY: ', curr_time[2])
+
