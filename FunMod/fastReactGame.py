58354a58930b3b87dda15369e0958a3925593468
Martin <bekzodsanakulov89@gmail.com>
2021-11-01 19:04:04 +0900

    fix: time formats on python

--- FunMod/fastReactGame.py
+++ FunMod/fastReactGame.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-ranInt = random.randint(0, 10)
+ranInt = random.randint(0, 3)
 # print(ranInt)
 
 time.sleep(ranInt)
@@ -10,6 +10,6 @@ input('Type as quick as you can')
 end_time = time.time()
 
 print('='*40)
-print('Start time at: ', time.strftime("%X", time.localtime(start_time)))
-print('Your pressed time at: ', time.strftime("%X", time.localtime(end_time)))
+print('Start time at: ', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time)))
+print('Your pressed time at: ', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(end_time)))
 print('Your reaction: ', end_time - start_time)
