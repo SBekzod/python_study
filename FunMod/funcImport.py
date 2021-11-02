8b57ef13f3a41083df4e5d61e010413a286cabcc
Martin <bekzodsanakulov89@gmail.com>
2021-11-02 22:26:20 +0900

    feat: import file modules and use __name__ builtins if something should not be executed in case import

---
+++ FunMod/funcImport.py
@@ -0,0 +1,5 @@
+import testFunctions
+
+print('hello')
+print(__name__)
+testFunctions.count_till_ten('function is run outside', 'test')
--- FunMod/testFunctions.py
+++ FunMod/testFunctions.py
@@ -1,13 +1,16 @@
 from time import sleep
+print('file name __name__ :', __name__)
+print('Testing Functions on Python file')
 
-print('Testing Functions on Python')
 
-
-def count_till_ten(*par, separ=":", end='', file=None, flush=False):
+def count_till_ten(*par, separate=":", end='', file=None, flush=False):
     print(par)
     for i in range(1, 11):
-        print(i, separ)
+        print(i, separate)
         sleep(1)
 
 
-count_till_ten('function is started', "you")
+# controls and commutes if the source is run inside
+if __name__ == '__main__':
+    count_till_ten('function is started', "you")
+    print('PERFECT')
