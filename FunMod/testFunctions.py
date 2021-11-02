2c08488d4e431565c023fa17e908ab676d727c5a
Martin <bekzodsanakulov89@gmail.com>
2021-11-02 21:24:15 +0900

    feat: functions with tupled parameters

--- FunMod/testFunctions.py
+++ FunMod/testFunctions.py
@@ -3,10 +3,11 @@ from time import sleep
 print('Testing Functions on Python')
 
 
-def count_till_ten():
+def count_till_ten(*par, separ=":", end='', file=None, flush=False):
+    print(par)
     for i in range(1, 11):
-        print(i)
+        print(i, separ)
         sleep(1)
 
 
-count_till_ten()
+count_till_ten('function is started', "you")
