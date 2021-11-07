6e95fa2beda8ff59042cb2f158e1f1e7f4deb2c8
martin <martin@comets.kr>
2021-11-08 01:14:45 +0900

    feat: build Python Class with constructor and private & public state, methods

---
+++ ClassOper/buildClass.py
@@ -0,0 +1,47 @@
+class Person:
+
+    def __init__(self, name, age, nation, is_married, secrets):
+        self.name = name
+        self.age = age
+        self.nation = nation
+        self.is_married = is_married
+        self.__secret_message = secrets
+
+    def get_jump(self, height):
+        print('{0} jumped with {1} metres'.format(self.name, height))
+
+    def get_swim(self):
+        print('{0} started to swim'.format(self.name))
+
+    def change_your_name(self, new_name):
+        print('{0} changed own name into {1}'.format(self.name, new_name))
+        self.name = new_name
+
+    def make_greeting(self):
+        print('Hi, my name is {0} and I am {1} years-old.'.format(self.name, self.age))
+
+    def reveal_message(self):
+        return self.__secret_message
+
+    def __change_message(self, new_secrets):
+        self.__secret_message = new_secrets
+        print('Message is changed into {0}'.format(self.__secret_message))
+        return self.__secret_message
+
+
+person = Person("David", 32, 'uzbek', True, 'apple')
+print('=' * 41)
+person.make_greeting()
+print('=' * 41)
+person.change_your_name('Patrick')
+print('=' * 41)
+person.make_greeting()
+print()
+print()
+
+print('**** second phase started **** ')
+print(person.name)                   # name is obtainable
+# print(person.__secret_message)       # is not achievable
+
+print('The secret message is {0}'.format(person.reveal_message()))  # name is obtainable
+# print(person.__change_message('cherry'))  # is not achievable
